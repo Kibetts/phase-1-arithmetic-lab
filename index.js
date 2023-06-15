08770c7acd5dda965bc87bd5f6becca0dba8a623
Brian Kibet <kibetsbrian@gmail.com>
2023-06-15 12:39:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * 40) + 1;
+console.log(random)
+
+let num3 = 24;
+let num4 = 20;
+const mod = num3 % num4;
+console.log(mod)
+
+const max = Math.max(2, 14, 20, 11, 15, 7);
+console.log(max)
